c96d471faa2d2c79a1cb5dec844b648286f4546d
AntonyKhalt0 <toscha.xalturin@gmail.com>
2023-04-19 18:52:48 -0700

    T1 Added devise

---
+++ app/models/user.rb
@@ -0,0 +1,17 @@
+class User < ApplicationRecord
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+         
+  PASSWORD_FORMAT = /\A
+    (?=.*\d)           # Must contain a digit
+    (?=.*[a-z])        # Must contain a lower case character
+    (?=.*[[:^alnum:]]) # Must contain a symbol
+  /x
+
+  validates :email, length: { maximum: 15 }
+  validates :password,
+    length: { in: 8..30 },
+    format: { with: PASSWORD_FORMAT, message: "должен содержать буквы, цифры, и спецсимволы!" },
+    confirmation: true
+  validates :password_confirmation, presence: true
+end
